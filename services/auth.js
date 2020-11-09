3d3d5f558ed163fc52d325564564482710b30c75
Noemi <nchoj08@gmail.com>
2020-11-08 18:41:01 -0600

    Commit json and auth users

---
+++ services/auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const { has } = require('mongoose/lib/helpers/specialProperties');
+
+var authService = {
+    signUser: function(user) {
+        const token = jwt.sign(
+            {
+                Username: user.Username,
+                UserId: user.UserId,
+                expiresIn: 10000
+            },
+            'secretkey',
+            {
+                expresIn: '1h'
+            },
+            (err, token) => {
+                if (err) throw err;
+                res.status(200).json({
+                    token
+                });
+            }
+        );
+    }
+}
+
+module.exports = authService;
