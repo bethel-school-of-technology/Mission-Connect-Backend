149f734bfc815bbb7e3eaafb16805bfe57704e17
Noemi <nchoj08@gmail.com>
2020-11-08 19:06:29 -0600

    Commit UserInfo

---
+++ models/UserInfo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+ var UserInfoSchema = new mongoose.Schema({
+     info: {
+            userId: Number,
+            firstName: {
+                type: String,
+                required: true
+            },
+            lastName: {
+                type: String,
+                required: true
+            },
+            email: {
+                type: String,
+                required: true
+            },
+            username: {
+                type: String, 
+                required: true
+            },
+            password: {
+                type: String,
+                required: true
+            },
+     },
+         createdAt: {
+             type: Date,
+             default: Date.now()
+         }
+ });
+ 
+
+module.exports = mongoose.model('UserInfo', UserInfoSchema);
